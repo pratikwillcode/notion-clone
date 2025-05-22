7f59554c3af3ade14d2b3846961d0e009b2f05bf
Pratik Awari <64844000+pratikwillcode@users.noreply.github.com>
2025-05-22 15:14:29 +0530

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -5,13 +5,25 @@ const serviceKey = require("@/service_key.json");
 
 let app: App;
 
-if(getApps().length === 0){
+// if(getApps().length === 0){
+//     app = initializeApp({
+//         credential: cert(serviceKey)
+//     })
+// } else{
+//     app = getApp();
+// }
+
+if (getApps().length === 0) {
     app = initializeApp({
-        credential: cert(serviceKey)
-    })
-} else{
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  } else {
     app = getApp();
-}
+  }
 
 const adminDb = getFirestore(app);
 
